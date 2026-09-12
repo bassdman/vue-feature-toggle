43e4c3fd1e0c24d5abd08e703829c717ecd003d5
Manuel Gelsen <github@manuelgelsen.de>
2026-09-12 22:49:21 +0200

    Improved Types

--- src/index.ts
+++ src/index.ts
@@ -1,51 +1,57 @@
-import { computed, defineComponent, h, ref } from 'vue';
-import {useFeatureToggle} from 'feature-toggle-api';
+import { computed, defineComponent, h, ref, type PropType } from 'vue';
+import { useFeatureToggle, type FeatureToggleConfig } from 'feature-toggle-api';
 
-export const featureToggle = useFeatureToggle();
-const visibilityRevision = ref(0);
+export function createFeatureToggle(config: FeatureToggleConfig = {}) {
+    const featureToggle = useFeatureToggle(config);
+    const visibilityRevision = ref(0);
 
-featureToggle.on('visibilityrule', () => {
-    visibilityRevision.value += 1;
-}, { ignorePreviousRules: true });
+    featureToggle.on('visibilityrule', () => {
+        visibilityRevision.value += 1;
+    }, { ignorePreviousRules: true });
 
-export const Feature = defineComponent({
-    name: 'Feature',
-    props: {
-        name: {
-            type: String,
-            required: true
-        },
-        variant: {
-            type: String
-        },
-        data: {
-            type: [Object, String]
-        },
-        tag: {
-            type: String,
-            default: ''
-        }
-    },
-    setup(props, { slots }) {
-        const isVisible = computed(() => {
-            // Let Vue know that the computed value depends on feature rule changes.
-            visibilityRevision.value;
-            return featureToggle.isActive(props.name, props.variant, props.data);
-        });
-
-        return () => {
-            if (!isVisible.value)
-                return null;
-
-            if (props.tag) {
-                return h(props.tag, {
-                    'feature-name': props.name,
-                    'feature-variant': props.variant
-                }, slots.default?.());
+    const Feature = defineComponent({
+        name: 'Feature',
+        props: {
+            name: {
+                type: String,
+                required: true
+            },
+            variant: {
+                type: String
+            },
+            data: {
+                type: null as unknown as PropType<unknown>
+            },
+            tag: {
+                type: String,
+                default: ''
             }
-            return slots.default?.() ?? null;
-        };
-    }
-});
+        },
+        setup(props, { slots }) {
+            const isVisible = computed(() => {
+                visibilityRevision.value;
+                return featureToggle.isActive(props.name, props.variant, props.data);
+            });
 
+            return () => {
+                if (!isVisible.value)
+                    return null;
+
+                if (props.tag) {
+                    return h(props.tag, {
+                        'feature-name': props.name,
+                        'feature-variant': props.variant
+                    }, slots.default?.());
+                }
+                return slots.default?.() ?? null;
+            };
+        }
+    });
+
+    return { featureToggle, Feature };
+}
+
+const defaultInstance = createFeatureToggle();
+export const featureToggle = defaultInstance.featureToggle;
+export const Feature = defaultInstance.Feature;
 export default featureToggle;
