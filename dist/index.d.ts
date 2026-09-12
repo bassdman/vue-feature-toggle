b315db172054bc00a44fb5a4749997affcf32568
Manuel Gelsen <github@manuelgelsen.de>
2026-09-12 22:49:11 +0200

    Added Factory-Export

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,3 +1,45 @@
+import { PropType } from 'vue';
+import { FeatureToggleConfig } from 'feature-toggle-api';
+export declare function createFeatureToggle(config?: FeatureToggleConfig): {
+    featureToggle: import('feature-toggle-api').FeatureToggleApi;
+    Feature: import('vue').DefineComponent<import('vue').ExtractPropTypes<{
+        name: {
+            type: StringConstructor;
+            required: true;
+        };
+        variant: {
+            type: StringConstructor;
+        };
+        data: {
+            type: PropType<unknown>;
+        };
+        tag: {
+            type: StringConstructor;
+            default: string;
+        };
+    }>, () => import('vue').VNode<import('vue').RendererNode, import('vue').RendererElement, {
+        [key: string]: any;
+    }> | import('vue').VNode<import('vue').RendererNode, import('vue').RendererElement, {
+        [key: string]: any;
+    }>[], {}, {}, {}, import('vue').ComponentOptionsMixin, import('vue').ComponentOptionsMixin, {}, string, import('vue').PublicProps, Readonly<import('vue').ExtractPropTypes<{
+        name: {
+            type: StringConstructor;
+            required: true;
+        };
+        variant: {
+            type: StringConstructor;
+        };
+        data: {
+            type: PropType<unknown>;
+        };
+        tag: {
+            type: StringConstructor;
+            default: string;
+        };
+    }>> & Readonly<{}>, {
+        tag: string;
+    }, {}, {}, {}, string, import('vue').ComponentProvideOptions, true, {}, any>;
+};
 export declare const featureToggle: import('feature-toggle-api').FeatureToggleApi;
 export declare const Feature: import('vue').DefineComponent<import('vue').ExtractPropTypes<{
     name: {
@@ -8,7 +50,7 @@ export declare const Feature: import('vue').DefineComponent<import('vue').Extrac
         type: StringConstructor;
     };
     data: {
-        type: (StringConstructor | ObjectConstructor)[];
+        type: PropType<unknown>;
     };
     tag: {
         type: StringConstructor;
@@ -27,7 +69,7 @@ export declare const Feature: import('vue').DefineComponent<import('vue').Extrac
         type: StringConstructor;
     };
     data: {
-        type: (StringConstructor | ObjectConstructor)[];
+        type: PropType<unknown>;
     };
     tag: {
         type: StringConstructor;
--- dist/vue-feature-toggle.js
+++ dist/vue-feature-toggle.js
@@ -1,33 +1,38 @@
 import { computed as e, defineComponent as t, h as n, ref as r } from "vue";
 import { useFeatureToggle as i } from "feature-toggle-api";
 //#region src/index.ts
-var a = i(), o = r(0);
-a.on("visibilityrule", () => {
-	o.value += 1;
-}, { ignorePreviousRules: !0 });
-var s = t({
-	name: "Feature",
-	props: {
-		name: {
-			type: String,
-			required: !0
-		},
-		variant: { type: String },
-		data: { type: [Object, String] },
-		tag: {
-			type: String,
-			default: ""
-		}
-	},
-	setup(t, { slots: r }) {
-		let i = e(() => (o.value, a.isActive(t.name, t.variant, t.data)));
-		return () => i.value ? t.tag ? n(t.tag, {
-			"feature-name": t.name,
-			"feature-variant": t.variant
-		}, r.default?.()) : r.default?.() ?? null : null;
-	}
-});
+function a(a = {}) {
+	let o = i(a), s = r(0);
+	return o.on("visibilityrule", () => {
+		s.value += 1;
+	}, { ignorePreviousRules: !0 }), {
+		featureToggle: o,
+		Feature: t({
+			name: "Feature",
+			props: {
+				name: {
+					type: String,
+					required: !0
+				},
+				variant: { type: String },
+				data: { type: null },
+				tag: {
+					type: String,
+					default: ""
+				}
+			},
+			setup(t, { slots: r }) {
+				let i = e(() => (s.value, o.isActive(t.name, t.variant, t.data)));
+				return () => i.value ? t.tag ? n(t.tag, {
+					"feature-name": t.name,
+					"feature-variant": t.variant
+				}, r.default?.()) : r.default?.() ?? null : null;
+			}
+		})
+	};
+}
+var o = a(), s = o.featureToggle, c = o.Feature;
 //#endregion
-export { s as Feature, a as default, a as featureToggle };
+export { c as Feature, a as createFeatureToggle, s as default, s as featureToggle };
 
 //# sourceMappingURL=vue-feature-toggle.js.map
